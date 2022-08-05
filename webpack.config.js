51c8c0d4922bd9b6d611a0d8098eacfc9b98a87d
IDDna <romanov1371@gmail.com>
2022-08-05 11:26:56 +0300

    Add icons to the project

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,6 @@ module.exports = {
    entry: path.resolve(__dirname, 'src/index.js'),
    output: {
       path: path.resolve(__dirname, 'dist'),
-      clean: true,
       assetModuleFilename: '[name][ext]',
    },
    devtool: 'inline-source-map',
